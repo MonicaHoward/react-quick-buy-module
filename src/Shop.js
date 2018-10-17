bf0966dd25e7a188958bf95f9aabc5f427da9dd0
Monica Howard <jay.m.howard@gmail.com>
2018-10-17 07:00:35 -0500

    Mapped over inventory

--- src/Shop.js
+++ src/Shop.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react';
 
 class Shop extends Component {
+    cartTotal(price){
+        this.setState({total: this.setState.total + price})
+    }
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            total: 0
+        };
+        this.cartTotal = this.cartTotal.bind(this)
+    }
     render(){
         console.log(this.props.items)
+        var inventory = this.props.items.map((item, i) => {
+            return <Item name={item.name}
+                         price={item.price}
+                         key={i}
+                         cartTotal={this.cartTotal}
+                         active={item.active}/>
+        });
         return(
             <div>
-                Shop
+                <h1>Shop</h1>
+                <div>
+                    {inventory}
+                    <p>Total: {this.state.total}</p>
+                </div>
             </div>
         );
+    }
+}
+
+class Item extends Component {
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
     }
 }
 
