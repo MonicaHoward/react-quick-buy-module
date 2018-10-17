8cf471f765f418423645378a8b30a3412fa7ebbe
Monica Howard <jay.m.howard@gmail.com>
2018-10-17 05:54:28 -0500

    Added inventory items

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import './App.css';
 
 class App extends Component {
   render() {
+    var courses = [
+        {name: 'Air Jordan 33 "Future Flight"\n', price: 175},
+        {name: 'Air Jordan 10 "Tinker"\n', price: 190},
+        {name: 'Air Jordan 10', price: 300},
+        {name: 'Air Jordan 11 "Platinum Tint"', price: 220},
+        {name: '88 Racer', price: 100},
+        {name: 'Hustle', price: 200},
+        {name: 'Air Jordan 1 Rebel XX', price: 200},
+        {name: 'Jordan Spiz\'ike', price: 175},
+        {name: 'Air Jordan 9 NRG Boot', price: 225}
+
+    ];
     return (
       <div className="App">
 
